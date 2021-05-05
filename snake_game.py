60a0afdc414fb9e202e1882cf296ae347569e8e3
EunYoung <pandamon24@naver.com>
2021-05-05 21:32:26 +0900

    전체적인 수정(배경, 캐릭터)

--- snake_game.py
+++ snake_game.py
@@ -1,49 +1,88 @@
 import pygame
 import random
 
-# 초기화
+# 게임 초기화
 pygame.init()
 
-# 화면 크기 설정
+# 스크린 그리기
 screen_width = 480
-screen_height = 640
+screen_height = 630
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# 화면 타이틀 설정
-pygame.display.set_caption("SNAKE")
+pygame.display.set_caption('Snake Game')
 
-# fps
 clock = pygame.time.Clock()
 
-# 배경, 캐릭터 설정
-background = pygame.image.load('C:/Users/user/Desktop/python_projects/snake_game/background.png')
+# 격자 그리기
+grid_pos1 = [30,30]
+grid_pos2 = [60,60]
+grid_pos3 = [60, 30]
+grid_pos4 = [30, 60]
 
+# 캐릭터 그리기
+snake_pos = [30*3, 30*10]
+snake_body = [[30, 30*10], [30*2, 30*10], [30*3, 30*10]]
 
-snake = pygame.image.load('C:/Users/user/Desktop/python_projects/snake_game/snake.png')
-snake_size = snake.get_rect().size
-snake_width = snake_size[0]
-snake_height = snake_size[1]
-snake_pos = [2*snake_width, screen_height/2 - snake_height/2] # 초기 위치
-snake_body = [[0, screen_height/2 - snake_height/2], [snake_width, screen_height/2 - snake_height/2], [2*snake_width, screen_height/2 - snake_height/2]] # 뱀 몸의 초기 위치
-
-food = pygame.image.load('C:/Users/user/Desktop/python_projects/snake_game/food.png')
-food_size = food.get_rect().size
-food_width = food_size[0]
-food_height = food_size[1]
-food_pos = [random.randrange(30, screen_width-30), random.randrange(30, screen_height-30)]
-
+fruit_pos = [random.randrange(30, screen_width-30,30),random.randrange(30, screen_height-30,30)]
 
 direction = 'right'
 score = 0
-font = pygame.font.SysFont('comicsans',40)
-#이벤트 루프
-running = True
+font = pygame.font.SysFont('comicsans', 40)
 
-while running:
-  dt = clock.tick(10)
+# 격자 그리기 함수
+def draw_backround():
+  for i in range(12):
+    for j in range(14):
+      pygame.draw.rect(screen,(196, 223, 100), (grid_pos1[0], grid_pos1[1], 30, 30))
+      grid_pos1[1] += 60
+      if grid_pos1[1] >= 600:
+        grid_pos1[1] = 30
+    pygame.draw.rect(screen,(196, 223, 100), (grid_pos1[0], grid_pos1[1], 30, 30))
+    grid_pos1[0] += 60
+    if grid_pos1[0] >= 450:
+      grid_pos1[0] = 30
+
+  for i in range(12):
+    for j in range(14):
+      pygame.draw.rect(screen,(196, 223, 100), (grid_pos2[0], grid_pos2[1], 30, 30))
+      grid_pos2[1] += 60
+      if grid_pos2[1] >= 600:
+        grid_pos2[1] = 60
+    pygame.draw.rect(screen,(196, 223, 100), (grid_pos2[0], grid_pos2[1], 30, 30))
+    grid_pos2[0] += 60
+    if grid_pos2[0] >= 450:
+      grid_pos2[0] = 60
+
+  for i in range(12):
+    for j in range(14):
+      pygame.draw.rect(screen, (156, 183, 68), (grid_pos3[0], grid_pos3[1], 30, 30))
+      grid_pos3[1] += 60
+      if grid_pos3[1] >= 600:
+        grid_pos3[1] = 60
+    pygame.draw.rect(screen, (156, 183, 68), (grid_pos3[0], grid_pos3[1], 30, 30))
+    grid_pos3[0] += 60
+    if grid_pos3[0] >= 450:
+      grid_pos3[0] = 30
+
+  for i in range(12):
+    for j in range(14):
+      pygame.draw.rect(screen, (156, 183, 68), (grid_pos4[0], grid_pos4[1], 30, 30))
+      grid_pos4[1] += 60
+      if grid_pos4[1] >= 600:
+        grid_pos4[1] = 30
+    pygame.draw.rect(screen, (156, 183, 68), (grid_pos4[0], grid_pos4[1], 30, 30))
+    grid_pos4[0] += 60
+    if grid_pos4[0] >= 450:
+      grid_pos4[0] = 60
+
+
+# 이벤트
+running = True
+while running :
+  clock.tick(10)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      running=False
+      running = False
 
     if event.type == pygame.KEYDOWN:
       if event.key == pygame.K_LEFT and direction != 'right':
@@ -54,48 +93,47 @@ while running:
         direction = 'up'
       elif event.key == pygame.K_DOWN and direction != 'up':
         direction = 'down'
- 
-  if direction == 'right':
-      snake_pos[0] += snake_width
-  elif direction == 'left':
-      snake_pos[0] -= snake_width
+
+  if direction == 'left':
+    snake_pos[0] -= 30
+  elif direction == 'right':
+    snake_pos[0] += 30
   elif direction == 'up':
-      snake_pos[1] -= snake_width
+    snake_pos[1] -= 30
   elif direction == 'down':
-      snake_pos[1] += snake_width
+    snake_pos[1] += 30
   
-  # 충돌처리
-  if snake_pos[0] + snake_width <= 0 or snake_pos[0] >= screen_width:
+  if snake_pos[0] <= 0 or snake_pos[0] >= screen_width-30:
     running = False
-  
-  if snake_pos[1] <= 0 or snake_pos[1] >= screen_height:
+  elif snake_pos[1] <=0 or snake_pos[1] >= screen_height-30:
     running = False
 
-  for square in snake_body[1:]:
-    if pygame.Rect(square[0], square[1], 30, 30).colliderect(pygame.Rect(snake_pos[0], snake_pos[1], 30, 30)):
+  for snake in snake_body[:-1]:
+    if pygame.Rect(snake_pos[0], snake_pos[1], 30, 30).colliderect(pygame.Rect(snake[0], snake[1], 30, 30)):
       running = False
+    
+  screen.fill((80, 120, 10))
+  draw_backround()
+  pygame.font 
+
+  for snake in snake_body:
+    pygame.draw.rect(screen, (100, 100, 100), (snake[0], snake[1], 30, 30))
   
-  # 그리기
-  screen.blit(background, (0, 0))
-
-  for square in snake_body:
-    screen.blit(snake, (square[0], square[1]))
-
-  screen.blit(food, (food_pos[0], food_pos[1]))
+  pygame.draw.rect(screen, (50, 50, 50), (fruit_pos[0],fruit_pos[1],30, 30))
 
   snake_body.append(list(snake_pos))
-  
-
-  if pygame.Rect(snake_pos[0],snake_pos[1],30,30).colliderect(pygame.Rect(food_pos[0],food_pos[1],30,30)):
-    food_pos = [random.randrange(30, screen_width-30), random.randrange(30, screen_height-30)]
+  # 충돌
+  if pygame.Rect(snake_pos[0],snake_pos[1], 30, 30).colliderect(pygame.Rect(fruit_pos[0],fruit_pos[1], 30, 30)) :
+    fruit_pos = [random.randrange(30, screen_width-30,30),random.randrange(30, screen_height-30,30)]
     score += 5
+    if fruit_pos in snake_body:
+      fruit_pos = [random.randrange(30, screen_width-30,30),random.randrange(30, screen_height-30,30)]
   else:
     snake_body.pop(0)
 
-  score_font = font.render(f'{score}' , True , (255,255,255))
-  font_pos = score_font.get_rect(center=(screen_width/2 , 30))
-  screen.blit(score_font , font_pos)
-
+  score_font = font.render(f'score : {score}', True, (255, 255, 255))
+  font_pos = score_font.get_rect(center = (screen_width/2, 15))
+  screen.blit(score_font, font_pos)  
   pygame.display.update()
-    
+
 pygame.quit()
